e5184d1a8ec63af89018b7d5d894770b71a4b4b6
scarybunny1 <46290395+scarybunny1@users.noreply.github.com>
2023-07-24 22:59:01 +0530

    Framework ready

---
+++ Popup-sdk/Popup_sdk.h
@@ -0,0 +1,18 @@
+//
+//  Popup_sdk.h
+//  Popup-sdk
+//
+//  Created by Ayush Bhatt on 24/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Popup_sdk.
+FOUNDATION_EXPORT double Popup_sdkVersionNumber;
+
+//! Project version string for Popup_sdk.
+FOUNDATION_EXPORT const unsigned char Popup_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Popup_sdk/PublicHeader.h>
+
+
